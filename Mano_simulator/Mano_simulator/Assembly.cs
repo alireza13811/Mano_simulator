ff730608a1301bbd21911c916a088e9753c1ddc1
Alireza <92114140+alireza13811@users.noreply.github.com>
2023-06-26 11:29:01 +0330

    variables created

---
+++ Mano_simulator/Mano_simulator/Assembly.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mano_simulator
+{
+    internal class Assembly
+    {
+        bool[,] Memory = new bool[2096,16];
+        bool[] Ar = new bool[11];
+        
+    }
+}
