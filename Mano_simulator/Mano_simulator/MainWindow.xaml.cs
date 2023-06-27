242f27785c1d36b88a8e4ff61b9ce073479ac921
Nimazm <63556318+Nimazm@users.noreply.github.com>
2023-06-27 19:16:36 +0330

    binding

--- Mano_simulator/Mano_simulator/MainWindow.xaml.cs
+++ Mano_simulator/Mano_simulator/MainWindow.xaml.cs
@@ -24,15 +24,23 @@ namespace Mano_simulator
         {
             InitializeComponent();
             // create a for loop to add rows to data grid
-            for (int i = 0; i < 4096; i++)
+            for (int i = 0; i < 2048; i++)
             {
                 DataGridData data = new DataGridData();
-                data.Address = i.ToString("X");
-                data.Value = "0000";
+                data.Address = "0x"+i.ToString("X");
+                data.Value = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
                 Datamemmory.Items.Add(data);
                 
             }
-            
+            for (int i = 0; i < 128; i++)
+            {
+                DataGridData data = new DataGridData();
+                data.Address = "0x" + i.ToString("X");
+                data.Value = "";
+                Microprogrammemmory.Items.Add(data);
+
+            }
+
         }
         private void btnRun_Click(object sender, RoutedEventArgs e)
         {
