e501653867850a1647e8b92eb7f7c721c7462068
Alireza <92114140+alireza13811@users.noreply.github.com>
2023-06-28 18:16:33 +0330

    Update Assembly.cs

--- Mano_simulator/Mano_simulator/Assembly.cs
+++ Mano_simulator/Mano_simulator/Assembly.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Mano_simulator
 {
@@ -157,11 +159,11 @@ namespace Mano_simulator
             }
         }
 
-        public void LoadCAR(string[] words)
+        public void LoadCAR(string words)
         {
             for (int i = 0; i < 7; i++)
             {
-                if (words[1][i] == '1')
+                if (words[i] == '1')
                 {
                     CAR[i] = true;
                 }
@@ -416,13 +418,15 @@ namespace Mano_simulator
             }
         }
 
-        public void JUMP()
+        public void JUMP(string word)
         {
-            // Jump to the address in the AR
-            for (int i = 0; i < 11; i++)
+            int address = microprogramLabels[word];
+            string binary = Convert.ToString(address, 2);
+            while (binary.Length < 4)
             {
-                PC[i] = AR[i];
+                binary = "0" + binary;
             }
+            LoadCAR(binary);
         }
 
         public void ACTDR()
@@ -571,6 +575,102 @@ namespace Mano_simulator
         public void NOP()
         {
             // Do nothing
+        }
+
+        public void INCDR()
+        {
+            // Increment the contents of the DR by one
+            int carry = 1;
+            for (int i = 15; i >= 0; i--)
+            {
+                if (DR[i])
+                {
+                    if (carry == 1)
+                    {
+                        DR[i] = false;
+                        carry = 1;
+                    }
+                    else
+                    {
+                        DR[i] = true;
+                        carry = 0;
+                    }
+                }
+                else
+                {
+                    if (carry == 1)
+                    {
+                        DR[i] = true;
+                        carry = 0;
+                    }
+                    else
+                    {
+                        DR[i] = false;
+                        carry = 0;
+                    }
+                }
+            }
+        }
+
+        public void INCCAR()
+        {
+            // Increment the contents of the CAR by one
+            int carry = 1;
+            for (int i = 6; i >= 0; i--)
+            {
+                if (CAR[i])
+                {
+                    if (carry == 1)
+                    {
+                        CAR[i] = false;
+                        carry = 1;
+                    }
+                    else
+                    {
+                        CAR[i] = true;
+                        carry = 0;
+                    }
+                }
+                else
+                {
+                    if (carry == 1)
+                    {
+                        CAR[i] = true;
+                        carry = 0;
+                    }
+                    else
+                    {
+                        CAR[i] = false;
+                        carry = 0;
+                    }
+                }
+            }
+        }
+
+        public void CARTSBR()
+        {
+            // Transfer the contents of the CAR to the SBR
+            for (int i = 0; i < 7; i++)
+            {
+                SBR[i] = CAR[i];
+            }
+        }
+
+        public void SBRTCAR()
+        {
+            // Transfer the contents of the SBR to the CAR
+            for (int i = 0; i < 7; i++)
+            {
+                CAR[i] = SBR[i];
+            }
+        }
+
+        public void MAP()
+        {
+            for (int i = 2; i < 6; i++)
+            {
+
+            }
         }
 
         // functions to compile the assembly code
@@ -837,22 +937,201 @@ namespace Mano_simulator
                     microinstructions = microinstructions.Skip(1).ToArray();
                     for(int i = 0; i < operations.Length; i++)
                     {
-                        controlMemory[LC, i] = operations[i];
+                        controlMemory[LC, i] = operations[i].Trim();
                     }
                     for(int i = 0; i <  microinstructions.Length - 3; i++)
                     {
-                        controlMemory[LC, operations.Length+i] = microinstructions[0];
+                        controlMemory[LC, operations.Length+i] = microinstructions[0].Trim();
                         microinstructions = microinstructions.Skip(1).ToArray();
                     }
 
                     for(int i = 3; i < microinstructions.Length + 3; i++)
                     {
-                        controlMemory[LC, i] = microinstructions[i - 3];
+                        controlMemory[LC, i] = microinstructions[i - 3].Trim();
                     }
 
                 }
                 LC++;
             }
+        }
+
+        public void run()
+        {
+            int address = microprogramLabels["FETCH"];
+            // convert address to binary string
+            string binary = Convert.ToString(address, 2);
+            while (binary.Length < 4)
+            {
+                binary = "0" + binary;
+            }
+            LoadCAR(binary);
+
+        }
+
+        public void execute_line_of_code()
+        {
+            int address = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                address += (int)Math.Pow(2, 6 - i) * Convert.ToInt32(CAR[i]);
+            }
+
+            string[] microinstructions = new string[6];
+            for (int i = 0; i < 6; i++)
+            {
+                microinstructions[i] = controlMemory[address, i]; 
+            }
+
+            string operation;
+            for (int i = 0; i < 3; i++)
+            {
+                operation = microinstructions[i];
+                if (operation == "")
+                    continue;
+                switch (operation)
+                {
+                    case "NOP":
+                        NOP();
+                        break;
+                    case "JMP":
+                        JUMP();
+                        break;
+                    case "CLRAC":
+                        CLRAC();
+                        break;
+                    case "INCAC":
+                        INCAC();
+                        break;
+                    case "DRTAC":
+                        DRTAC();
+                        break;
+                    case "DRTAR":
+                        DRTAR();
+                        break;
+                    case "PCTAR":
+                        PCTAR();
+                        break;
+                    case "WRITE":
+                        WRITE();
+                        break;
+                    case "READ":
+                        READ();
+                        break;
+                    case "SUB":
+                        SUB();
+                        break;
+                    case "OR":
+                        OR();
+                        break;
+                    case "AND":
+                        AND();
+                        break;
+                    case "ACTDR":
+                        ACTDR();
+                        break;
+                    case "INCDR":
+                        INCDR();
+                        break;
+                    case "PCTDR":
+                        PCTDR();
+                        break;
+                    case "XOR":
+                        XOR();
+                        break;
+                    case "COM":
+                        COM();
+                        break;
+                    case "SHL":
+                        SHL();
+                        break;
+                    case "SHR":
+                        SHR();
+                        break;
+                    case "INCPC":
+                        INCPC();
+                        break;
+                    case "ARTPC":
+                        ARTPC();
+                        break;
+
+                    default:
+                        throw new ArgumentException("Invalid operation");
+
+                }
+
+            }
+
+            string condition = microinstructions[3];
+            bool flag = true;
+            switch (condition)
+            {
+                case "I":
+                    flag = DR[0];
+                    break;
+                case "Z":
+                    flag = IsACZero();
+                    break;
+                case "S":
+                    flag = AC[0];
+                    break;
+
+            }
+
+            if (flag)
+            {
+                string BR = microinstructions[4];
+                string dest = microinstructions[5];
+                switch (BR)
+                {
+                    case "JMP":
+                        {
+                            if(dest == "NEXT")
+                            {
+                                INCCAR();
+                            }
+                            else
+                            {
+                                JUMP(dest);
+                            }
+                            break;
+                        }
+                    case "CALL":
+                        {
+                            INCCAR();
+                            CARTSBR();
+                            JUMP(dest);
+                            break;
+                        }
+                    case "RET":
+                        {
+                            SBRTCAR();
+                            break;
+                        }
+                    case "MAP":
+                        {
+                            MAP();
+                            break;
+                        }
+                     
+
+                }
+            }
+
+            
+        }
+
+        public bool IsACZero()
+        {
+            bool flag = true;
+            for (int i = 0; i < 16; i++)
+            {
+                if (AC[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
         }
 
     }
